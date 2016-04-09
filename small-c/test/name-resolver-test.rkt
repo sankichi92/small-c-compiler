e2f3c423e85707fdcef6c35fa38cc93d6d233f8a
Sankichi <info@sankichi.net>
2016-04-09 23:52:05 +0900

    Add some test

--- small-c/test/name-resolver-test.rkt
+++ small-c/test/name-resolver-test.rkt
@@ -1,12 +1,22 @@
 #lang racket/base
 (require rackunit
-         (prefix-in stx: "../src/syntax.rkt")
-         "../src/parser.rkt")
+         "../src/syntax.rkt"
+         "../src/parser.rkt"
+         "../src/name-resolver.rkt")
 (require rackunit/text-ui)
+(provide name-resolver)
 
 (define name-resolver-tests
   (test-suite
     "Tests for name-resolver.rkt"
+
+    (check-equal?
+      (name-resolver
+        (list
+          (var-decl 'a 'int '())))
+      (list
+        (var-decl (decl 'a 0 'var 'int) 'int '())))
+
     ))
 
 (run-tests name-resolver-tests)
--- small-c/test/parser-test.rkt
+++ small-c/test/parser-test.rkt
@@ -1,12 +1,57 @@
 #lang racket/base
 (require rackunit
-         (prefix-in stx: "../src/syntax.rkt")
+         parser-tools/lex
+         parser-tools/yacc
+         "../src/syntax.rkt"
          "../src/parser.rkt")
 (require rackunit/text-ui)
+(provide parser-tests)
 
 (define parser-tests
   (test-suite
     "Tests for parser.rkt"
-    ))
+
+    (check-equal?
+      (parse-string "int a;")
+      (list
+        (fun-decl 'print 'void '(int) '())
+        (var-decl 'a 'int (position 5 1 4)))
+      "var-decl")
+
+    (check-equal?
+      (cdr (parse-string "int a();"))
+      (list
+        (fun-decl 'a 'int '() (position 1 1 0)))
+      "fun-decl")
+
+    (check-equal?
+      (cdr (parse-string "void a(){;}"))
+      (list
+        (fun-def
+          'a
+          'void
+          '()
+          (cmpd-stmt '() '(()) (position 9 1 8))
+          (position 1 1 0)))
+      "fun-def, cmpd-stmt")
+
+    (check-equal?
+      (cdr (parse-string "int a(int b){return b;}"))
+      (list
+        (fun-def
+          'a
+          'int
+          (list (parm-decl 'b 'int (position 11 1 10)))
+          (cmpd-stmt
+            '()
+            (list
+              (ret-stmt
+                (list
+                  (var-exp 'b (position 21 1 20)))
+                (position 14 1 13)))
+            (position 13 1 12))
+          (position 1 1 0)))
+      "fun-def, cmpd-stmt")
+  ))
 
 (run-tests parser-tests)
