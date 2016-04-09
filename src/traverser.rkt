85655b993e20435981182c6a20fe6329eec428be
Sankichi <info@sankichi.net>
2016-04-10 02:09:08 +0900

    Implement traverse-stmt

--- src/traverser.rkt
+++ src/traverser.rkt
@@ -28,9 +28,27 @@
                         [new-decls (map traverse-decl decls)]
                         [new-stmts (map traverse-stmt stmts)])
                    (stx:cmpd-stmt new-decls new-stmts pos))]
-                [(stx:if-els-stmt? stmt) stmt]
-                [(stx:while-stmt? stmt) stmt]
-                [(stx:ret-stmt? stmt) stmt])])
+                [(stx:if-els-stmt? stmt)
+                 (let* ([test (stx:if-els-stmt-test stmt)]
+                        [tbody (stx:if-els-stmt-tbody stmt)]
+                        [ebody (stx:if-els-stmt-ebody stmt)]
+                        [pos (stx:if-els-stmt-pos stmt)]
+                        [new-test (map traverse-exp test)]
+                        [new-tbody (map traverse-stmt tbody)]
+                        [new-ebody (map traverse-stmt ebody)])
+                   (stx:if-els-stmt new-test new-tbody new-ebody pos))]
+                [(stx:while-stmt? stmt)
+                 (let* ([test (stx:while-stmt-test stmt)]
+                        [body (stx:while-stmt-body stmt)]
+                        [pos (stx:while-stmt-pos stmt)]
+                        [new-test (map traverse-exp test)]
+                        [new-body (map traverse-stmt body)])
+                    (stx:while-stmt new-test new-body pos))]
+                [(stx:ret-stmt? stmt)
+                 (let* ([exp (stx:ret-stmt-exp stmt)]
+                        [pos (stx:ret-stmt-pos stmt)]
+                        [new-exp (map traverse-exp exp)])
+                   (stx:ret-stmt new-exp pos))])])
       (stmt-proc new-stmt)))
   (define (traverse-exp exp) exp)
   (map traverse-decl ast))
