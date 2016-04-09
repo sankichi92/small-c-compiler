6800ca6d6866dcd9305a4d74f7c98b708b9a8b44
Sankichi <info@sankichi.net>
2016-04-10 01:55:27 +0900

    Implement the traverser for test 3

--- src/traverser.rkt
+++ src/traverser.rkt
@@ -5,17 +5,32 @@
 
 (define (traverser decl-proc stmt-proc exp-proc ast)
   (define (traverse-decl decl)
-    (cond [(stx:var-decl? decl) decl]
-          [(stx:fun-decl? decl) decl]
-          [(stx:fun-def? decl)
-           (let* ([name (stx:fun-def-name decl)]
-                  [ret-ty (stx:fun-def-ret-ty decl)]
-                  [parms (stx:fun-def-parms decl)]
-                  [body (stx:fun-def-body decl)]
-                  [pos (stx:fun-def-pos decl)]
-                  [new-params (map decl-proc parms)]
-                  [new-body (traverse-stmt body)])
-              (stx:fun-def name ret-ty new-params new-body pos))]
-          [(stx:parm-decl? decl) decl]))
-  (define (traverse-stmt stmt) stmt)
-  (map decl-proc (map traverse-decl ast)))
+    (let ([new-decl
+           (cond [(stx:fun-def? decl)
+                  (let* ([name (stx:fun-def-name decl)]
+                         [ret-ty (stx:fun-def-ret-ty decl)]
+                         [parms (stx:fun-def-parms decl)]
+                         [body (stx:fun-def-body decl)]
+                         [pos (stx:fun-def-pos decl)]
+                         [new-params (map traverse-decl parms)]
+                         [new-body (traverse-stmt body)])
+                    (stx:fun-def name ret-ty new-params new-body pos))]
+                 [else decl])])
+      (decl-proc new-decl)))
+  (define (traverse-stmt stmt)
+    (let ([new-stmt
+          (cond [(null? stmt) '()]
+                [(list? stmt) (map traverse-exp stmt)]
+                [(stx:cmpd-stmt? stmt)
+                 (let* ([decls (stx:cmpd-stmt-decls stmt)]
+                        [stmts (stx:cmpd-stmt-stmts stmt)]
+                        [pos (stx:cmpd-stmt-pos stmt)]
+                        [new-decls (map traverse-decl decls)]
+                        [new-stmts (map traverse-stmt stmts)])
+                   (stx:cmpd-stmt new-decls new-stmts pos))]
+                [(stx:if-els-stmt? stmt) stmt]
+                [(stx:while-stmt? stmt) stmt]
+                [(stx:ret-stmt? stmt) stmt])])
+      (stmt-proc new-stmt)))
+  (define (traverse-exp exp) exp)
+  (map traverse-decl ast))
--- test/traverser-test.rkt
+++ test/traverser-test.rkt
@@ -43,14 +43,46 @@
           '()))
       "2")
 
+    (check-equal?
+      (traverser
+        decl-proc
+        stmt-proc
+        exp-proc
+        (list
+          (fun-def
+            'a
+            'int
+            (list (parm-decl 'b 'int '()))
+            (cmpd-stmt
+             '()
+             (list (ret-stmt (list (var-exp 'b '())) '()))
+             '())
+            '())))
+      (list
+        (fun-def
+          'a
+          'int
+          (list 'b)
+          (cmpd-stmt
+           '()
+           (list (ret-stmt (list (var-exp 'b '())) (position 7 7 7)))
+           '())
+          '()))
+      "3")
+
   ))
 
 (define (decl-proc decl)
   (cond [(var-decl? decl) (var-decl-name decl)]
         [(fun-def? decl) decl]
-        [(parm-decl? decl) (parm-decl-name decl)]))
+        [(parm-decl? decl) (parm-decl-name decl)]
+        [else decl]))
 
-(define (stmt-proc stmt) stmt)
+(define (stmt-proc stmt)
+  (cond [(cmpd-stmt? stmt) stmt]
+        [(ret-stmt? stmt)
+         (ret-stmt (ret-stmt-exp stmt) (position 7 7 7))]
+        [else stmt]))
 
 (define (exp-proc exp) exp)
 
