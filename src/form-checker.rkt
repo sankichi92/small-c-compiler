0425f5a9297c69acced0fef58b97566e2077b887
Sankichi <info@sankichi.net>
2016-04-10 12:31:47 +0900

    Add a template for form-checker

--- src/form-checker.rkt
+++ src/form-checker.rkt
@@ -1,5 +1,16 @@
 lang racket
 (require parser-tools/lex
          (prefix-in stx: "syntax.rkt")
-         (prefix-in nr: "name-resolver.rkt"))
-(provide (all-defined-out))
+         "utils.rkt"
+         "traverser.rkt"
+         "name-resolver.rkt")
+(provide form-check)
+
+(define (form-check ast)
+  (define (check-decl decl) decl)
+  (define (check-stmt stmt) stmt)
+  (define (check-exp exp)
+    (cond [(stx:assign-exp? exp) exp]
+          [(stx:addr-exp? exp) exp]
+          [else exp]))
+  (traverse check-decl check-stmt check-exp ast))
