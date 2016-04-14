414082b30c383918416af05ba55d8fb8d334f574
Sankichi <info@sankichi.net>
2016-04-14 14:42:04 +0900

    Add 'fresh-obj' and 'fresh-label' functions

--- src/ir-generator.rkt
+++ src/ir-generator.rkt
@@ -9,4 +9,15 @@ lang racket
 (provide ast->ir)
 
 (define (ast->ir ast)
-  )
+  (let ([var-maxid 0]
+        [label-maxid 0])
+    (define (fresh-obj)
+      (let* ([oldid var-maxid]
+             [sym-str (string-append "_x" (number->string oldid))]
+             [sym (string->symbol sym-str)])
+        (set! var-maxid (add1 var-maxid))
+        (ett:decl sym '() 'temp 'temp)))
+    (define (fresh-label)
+      (let ([oldid label-maxid])
+        (set! label-maxid (+ label-maxid 1))
+        (string-append "label" (number->string oldid))))))
