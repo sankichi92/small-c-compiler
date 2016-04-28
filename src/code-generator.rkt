7145e690dde1051241760892e7b731b704938724
Sankichi <info@sankichi.net>
2016-04-28 16:33:49 +0900

    Add asm.rkt

--- src/code-generator.rkt
+++ src/code-generator.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (require (prefix-in ett: "entity.rkt")
          (prefix-in ir:  "ir.rkt")
+         "asm.rkt"
          "utils.rkt"
          "addr-assigner.rkt")
 (provide ir->code code->string)
