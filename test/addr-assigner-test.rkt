a78f4064570b1da93432fc02b56a6e207e8efd64
Sankichi <info@sankichi.net>
2016-04-28 11:54:39 +0900

    Implement addr-assigner

--- test/addr-assigner-test.rkt
+++ test/addr-assigner-test.rkt
@@ -4,11 +4,42 @@
          "../src/ir.rkt"
          "../src/addr-assigner.rkt")
 (require rackunit/text-ui)
-(provide ir-generator-tests)
+(provide addr-assigner-tests)
 
 (define addr-assigner-tests
   (test-suite
     "Tests for addr-assigner.rkt"
+
+    (check-equal?
+      (cdr (addr-assign-str "void f(int a,int b,int c,int *d,int *e,int *f){int x,y[4];;}"))
+      (list
+       (fun-def
+        (decl
+         'f
+         0
+         'fun
+         '(fun void int int int (pointer int) (pointer int) (pointer int)))
+        (list
+         (var-decl (decl 'a 1 'parm 'int))
+         (var-decl (decl 'b 1 'parm 'int))
+         (var-decl (decl 'c 1 'parm 'int))
+         (var-decl (decl 'd 1 'parm '(pointer int)))
+         (var-decl (let ([obj (decl 'e 1 'parm '(pointer int))])
+                     (set-decl-offset! obj 4)
+                     obj))
+         (var-decl (let ([obj (decl 'f 1 'parm '(pointer int))])
+                     (set-decl-offset! obj 8)
+                     obj)))
+        (cmpd-stmt
+         (list
+          (var-decl (let ([obj (decl 'x 2 'var 'int)])
+                      (set-decl-offset! obj 0)
+                      obj))
+          (var-decl (let ([obj (decl 'y 2 'var '(array int 4))])
+                      (set-decl-offset! obj -16)
+                      obj)))
+         '())))
+      "Simple")
 
     ))
 
