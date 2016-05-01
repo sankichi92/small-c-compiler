7df9311aeab9ce34330b5bad8052b52612512e1a
Sankichi <info@sankichi.net>
2016-05-01 15:06:40 +0900

    Fix addr-assigner-test

--- test/addr-assigner-test.rkt
+++ test/addr-assigner-test.rkt
@@ -14,34 +14,36 @@
     "Tests for addr-assigner.rkt"
 
     (check-equal?
-      (cdr (assign-string "void f(int a,int b,int c,int *d,int *e,int *f){int x,y[4];;}"))
+      (assign-string "void f(int a,int b,int c,int *d,int *e,int *f){int x,y[4];;}")
       (list
-       (fun-def
-        (decl
-         'f
-         0
-         'fun
-         '(fun void int int int (pointer int) (pointer int) (pointer int)))
-        (list
-         (var-decl (decl 'a 1 'parm 'int))
-         (var-decl (decl 'b 1 'parm 'int))
-         (var-decl (decl 'c 1 'parm 'int))
-         (var-decl (decl 'd 1 'parm '(pointer int)))
-         (var-decl (let ([obj (decl 'e 1 'parm '(pointer int))])
-                     (set-decl-offset! obj 4)
-                     obj))
-         (var-decl (let ([obj (decl 'f 1 'parm '(pointer int))])
-                     (set-decl-offset! obj 8)
-                     obj)))
-        (cmpd-stmt
-         (list
-          (var-decl (let ([obj (decl 'x 2 'var 'int)])
-                      (set-decl-offset! obj 0)
-                      obj))
-          (var-decl (let ([obj (decl 'y 2 'var '(array int 4))])
-                      (set-decl-offset! obj -16)
-                      obj)))
-         '())))
+       (cons
+         (fun-def
+          (decl
+           'f
+           0
+           'fun
+           '(fun void int int int (pointer int) (pointer int) (pointer int)))
+          (list
+           (var-decl (decl 'a 0 'parm 'int))
+           (var-decl (decl 'b 1 'parm 'int))
+           (var-decl (decl 'c 2 'parm 'int))
+           (var-decl (decl 'd 3 'parm '(pointer int)))
+           (var-decl (let ([obj (decl 'e 4 'parm '(pointer int))])
+                       (set-decl-offset! obj 4)
+                       obj))
+           (var-decl (let ([obj (decl 'f 5 'parm '(pointer int))])
+                       (set-decl-offset! obj 8)
+                       obj)))
+          (cmpd-stmt
+           (list
+            (var-decl (let ([obj (decl 'x 2 'var 'int)])
+                        (set-decl-offset! obj 0)
+                        obj))
+            (var-decl (let ([obj (decl 'y 2 'var '(array int 4))])
+                        (set-decl-offset! obj -16)
+                        obj)))
+           '()))
+         20))
       "Simple")
 
     ))
