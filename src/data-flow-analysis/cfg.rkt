cb9223e71fc2b9d3c75fccd9bd13ed3be85e81ad
Sankichi <info@sankichi.net>
2016-05-01 12:08:34 +0900

    Add 'basic block' definition

--- src/data-flow-analysis/cfg.rkt
+++ src/data-flow-analysis/cfg.rkt
@@ -1,4 +1,13 @@
+;;;; 制御フローグラフ(control flow graph)
 #lang racket
 (require (prefix-in ett: "../entity.rkt")
          (prefix-in ir: "../ir.rkt"))
 (provide (all-defined-out))
+
+;; 基本ブロック (basic block)
+(struct bblock
+  ((labels #:mutable) ; 先頭ラベル集合
+   stmts              ; ブロック内stmtのベクタ
+   (preds #:mutable)  ; predecessors (CFG中のインデックスの集合)
+   (succs #:mutable)) ; successors (CFG中のインデックスの集合)
+  #:transparent)
