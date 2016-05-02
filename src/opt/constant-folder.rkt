9f0da8e7824556e8183b99651826bca3105f8415
Sankichi <info@sankichi.net>
2016-05-02 16:09:23 +0900

    Fix an infinite loop bug of constant-folder

--- src/opt/constant-folder.rkt
+++ src/opt/constant-folder.rkt
@@ -20,59 +20,61 @@
          (let ([folded-stmts (map fold-stmt stmts)])
            (ir:cmpd-stmt decls folded-stmts))]
         [(ir:assign-stmt var exp)
-         (let* ([prop (dfa:get-property solution stmt #:kind 'before)]
-                [folded-exp (fold-exp prop exp)])
+         (let* ([folded-exp (fold-exp stmt exp)])
            (ir:assign-stmt var folded-exp))]
         [else stmt]))
-    (define (fold-exp prop exp)
-      (match exp
-        [(ir:var-exp var)
-         (let ([folded-var (fold-var prop var)])
-           (if folded-var
-               (ir:lit-exp folded-var)
-               exp))]
-        [(ir:aop-exp op left right)
-         (let ([l (fold-var prop left)]
-               [r (fold-var prop right)])
-           (if (and l r)
-               (let ([val (match op
-                            ['+ (+ l r)]
-                            ['- (- l r)]
-                            ['* (* l r)]
-                            ['/ (/ l r)])])
-                  (ir:lit-exp val))
-               exp))]
-        [(ir:rop-exp op left right)
-         (let ([l (fold-var prop left)]
-               [r (fold-var prop right)])
-           (if (and l r)
-               (let ([val (match op
-                            ['== (if (= l r) 1 0)]
-                            ['!= (if (= l r) 0 1)]
-                            ['<  (if (< l r) 1 0)]
-                            ['<= (if (<= l r) 1 0)]
-                            ['>  (if (> l r) 1 0)]
-                            ['>= (if (>= l r) 1 0)])])
-                 (ir:lit-exp val))
-               exp))]
-        [else exp]))
-    (define (fold-var prop var)
+    (define (fold-exp stmt exp [srcs (seteq)])
+      (let ([new-srcs (set-add srcs stmt)]
+            [prop (dfa:get-property solution stmt #:kind 'before)])
+        (match exp
+          [(ir:var-exp var)
+           (let ([folded-var (fold-var prop var new-srcs)])
+             (if folded-var
+                 (ir:lit-exp folded-var)
+                 exp))]
+          [(ir:aop-exp op left right)
+           (let ([l (fold-var prop left new-srcs)]
+                 [r (fold-var prop right new-srcs)])
+             (if (and l r)
+                 (let ([val (match op
+                              ['+ (+ l r)]
+                              ['- (- l r)]
+                              ['* (* l r)]
+                              ['/ (/ l r)])])
+                    (ir:lit-exp val))
+                 exp))]
+          [(ir:rop-exp op left right)
+           (let ([l (fold-var prop left new-srcs)]
+                 [r (fold-var prop right new-srcs)])
+             (if (and l r)
+                 (let ([val (match op
+                              ['== (if (= l r) 1 0)]
+                              ['!= (if (= l r) 0 1)]
+                              ['<  (if (< l r) 1 0)]
+                              ['<= (if (<= l r) 1 0)]
+                              ['>  (if (> l r) 1 0)]
+                              ['>= (if (>= l r) 1 0)])])
+                   (ir:lit-exp val))
+                 exp))]
+          [else exp])))
+    (define (fold-var prop var srcs)
       (let* ([dummy-defs (dict-ref prop var '())]
              [defs (set-remove dummy-defs 'dummy)])
         (if (= (set-count defs) 1)
             (let ([def (set-first defs)])
-              (match def
-                [(ir:assign-stmt _ _)
-                 (let* ([folded-astmt (fold-stmt def)]
-                        [folded-exp (ir:assign-stmt-exp folded-astmt)])
-                   (if (ir:lit-exp? folded-exp)
-                       (ir:lit-exp-val folded-exp)
-                       #f))]
-                [(ir:write-stmt _ src)
-                 (if (eq? (ett:decl-kind var) 'parm)
-                     #f
-                     (let ([prop (dfa:get-property solution def #:kind 'before)])
-                        (fold-var prop src)))]
-                ['any #f]))
+              (if (set-member? srcs def)
+                  #f
+                  (match def
+                    [(ir:assign-stmt _ exp)
+                     (let* ([folded-exp (fold-exp def exp srcs)])
+                       (if (ir:lit-exp? folded-exp)
+                           (ir:lit-exp-val folded-exp)
+                           #f))]
+                    [(ir:write-stmt _ src)
+                     (if (eq? (ett:decl-kind var) 'parm)
+                         #f
+                         (let ([prop (dfa:get-property solution def #:kind 'before)])
+                            (fold-var prop src)))]
+                    ['any #f])))
             #f)))
     (map fold-decl ir)))
